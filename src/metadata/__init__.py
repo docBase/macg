dae6929c0d27a618f0b2a62b457e4c614e1e405e
matias pizarro <matias@pizarro.net>
2016-03-25 00:05:18 +0100

    Improve metadata creation

--- src/metadata/__init__.py
+++ src/metadata/__init__.py
@@ -5,6 +5,7 @@ import pandas as pd
 import numpy as np
 
 pd.set_option('display.width', None)
+
 BASEDIR = os.path.dirname(os.path.abspath(__file__))
 DATADIR = os.path.join(BASEDIR, '..', '..', 'data')
 
@@ -17,10 +18,13 @@ def set_id(row):
     else:
         return "{0}.{1:0>2}".format(row['parents'], int(row['cat_order']))
 
+def get_metadata():
+    metadata = pd.read_csv(DATADIR + '/GCI_Dataset_2006-2015.metadata.csv')
+    fixed_ids = pd.read_csv(DATADIR + '/fixed_ids.csv')
+    metadata = metadata.merge(fixed_ids, how='left')
+    metadata['id'] = metadata.apply(lambda row: set_id(row), axis=1)
+    metadata.sort_values('id', inplace=True)
+    metadata.reset_index(inplace=True)
+    return metadata
 
-metadata = pd.read_csv(DATADIR + '/GCI_Dataset_2006-2015.metadata.csv')
-fixed_ids = pd.read_csv(DATADIR + '/fixed_ids.csv')
-metadata = metadata.merge(fixed_ids, how='left')
-metadata['id'] = metadata.apply(lambda row: set_id(row), axis=1)
-metadata.sort_values('id', inplace=True)
-metadata.reset_index(inplace=True)
+metadata = get_metadata()
